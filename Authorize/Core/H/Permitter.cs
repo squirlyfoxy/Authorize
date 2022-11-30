43cfaaccb95d620293f3d4aba2e89d14ba33bfcb
lbaldazzi <leonardo.baldazzi@horsa.it>
2022-11-30 11:40:36 +0100

    instance

--- Authorize/Core/H/Permitter.cs
+++ Authorize/Core/H/Permitter.cs
@@ -6,21 +6,41 @@ using System.Threading.Tasks;
 
 namespace Authorize.Core.H
 {
+    public enum PermitterClassManager
+    {
+        /// <summary>
+        /// Permitter will work as an instance, registering and deregistering classType from Manager
+        /// </summary>
+        Instance,
+        NoRegister
+    }
+
     public class Permitter : IDisposable
     {
-        public Permitter(Type classType, IPermitClass permit)
+        public Permitter(Type classType, IPermitClass permit, PermitterClassManager classManager = PermitterClassManager.NoRegister)
         {
             PermitClass = permit;
             ClassType = classType;
+            PermitterClassManager = classManager;
+
+            if (!Manager.ClassRegistered(classType) && classManager == PermitterClassManager.Instance)
+            {
+                Manager.RegisterClass(classType);
+            }
         }
 
         public Type ClassType { get; private set; }
 
         public IPermitClass PermitClass { get; private set; }
 
+        public PermitterClassManager PermitterClassManager { get; private set; }
+
         public void Dispose()
         {
-
+            if (PermitterClassManager == PermitterClassManager.Instance)
+            {
+                Manager.DeregisterClass(ClassType);
+            }
         }
 
         public dynamic Get(string propertyName)
@@ -44,11 +64,11 @@ namespace Authorize.Core.H
             return Class.Query(ClassType, linq, property, instance);
         }
 
-        public static Permitter Instance<T>(T instance) where T : IPermitClass
+        public static Permitter Instance<T>(T instance, PermitterClassManager classManager = PermitterClassManager.NoRegister) where T : IPermitClass
         {
             var type = typeof(T);
 
-            return new Permitter(type, instance);
+            return new Permitter(type, instance, classManager);
         }
     }
 }
